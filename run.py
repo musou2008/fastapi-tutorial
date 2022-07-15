600bb1fe2c23c22ce5a8ad8957a9061aad2945dd
musou2008 <deco.musou@gmail.com>
2022-07-16 00:16:55 +0800

    update

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ from tutorial import app03, app04, app05, app06, app07, app08
 app = FastAPI()
 
 app.include_router(app03, prefix="/chapter03",tags=["第3章 请求参数和验证"])
-app.include_router(app03, prefix="/chapter04",tags=["第4章 响应处理和FastAPI配置"])
-app.include_router(app03, prefix="/chapter05",tags=["第5章 FastAPI的依赖注入系统"])
-app.include_router(app03, prefix="/chapter06",tags=["第6章 安全、认证和授权"])
-app.include_router(app03, prefix="/chapter07",tags=["第7章 FastAPI的数据库操作和多应用的目录结构设计"])
-app.include_router(app03, prefix="/chapter08",tags=["第8章 中间件、CORS、后台任务、测试用例"])
+app.include_router(app04, prefix="/chapter04",tags=["第4章 响应处理和FastAPI配置"])
+app.include_router(app05, prefix="/chapter05",tags=["第5章 FastAPI的依赖注入系统"])
+app.include_router(app06, prefix="/chapter06",tags=["第6章 安全、认证和授权"])
+app.include_router(app07, prefix="/chapter07",tags=["第7章 FastAPI的数据库操作和多应用的目录结构设计"])
+app.include_router(app08, prefix="/chapter08",tags=["第8章 中间件、CORS、后台任务、测试用例"])
 
 if __name__ == "__main__":
     uvicorn.run(
--- tutorial/chapter04.py
+++ tutorial/chapter04.py
@@ -1,3 +1,46 @@
+from optparse import Option
+from typing import Optional, List
 from fastapi import APIRouter
+from pydantic import BaseModel, EmailStr
 
 app04 = APIRouter()
+
+"""Response Model 响应模型"""
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    mobile: str = "10086"
+    address: Optional[str] = None
+    full_name: Optional[str] = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr  # 需要pip install pydantic[email]
+    mobile: str = "10086"
+    address: Optional[str] = None
+    full_name: Optional[str] = None
+
+
+users = {
+    "user01": {
+        "username": "user01",
+        "password": "123123",
+        "email": "user01@example.com",
+    },
+    "user02": {
+        "username": "user02",
+        "password": "123456",
+        "email": "user02@example.com",
+        "mobile": "110",
+    },
+}
+# path operation
+@app04.post("/reponse_model", response_model=UserOut, response_model_exclude_unset=True)
+async def response_model(user: UserIn):
+    """response_model_exclude_unset=True表示默认值不包含在响应中，仅包含实际给的值，如果实际给的值与默认值相同，也会包含在响应中"""
+    print(user.password)  # password不会被返回
+    return users["user02"]
