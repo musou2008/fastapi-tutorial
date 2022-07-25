be9c2869438f7ec4aa62003b1a5c02f46a961bb5
musou2008 <deco.musou@gmail.com>
2022-07-25 17:13:09 +0800

    update

--- tutorial/chapter05.py
+++ tutorial/chapter05.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Header, HTTPException
 
 app05 = APIRouter()
 
@@ -43,3 +43,35 @@ async def classes_as_dependencies(commons=Depends(CommonQueryParams)):
     items = fake_items_db[commons.page : commons.page + commons.limit]
     response.update({"items": items})
     return response
+
+
+"""Sub-dependencies 子依赖"""
+
+
+def query(q: Optional[str] = None):
+    return q
+
+
+def sub_query(q: str = Depends(query), last_query: Optional[str] = None):
+    if not q:
+        return last_query
+
+    return q
+
+
+@app05.get("/sub_dependency")
+async def sub_dependency(final_query: str = Depends(sub_query, use_cache=True)):
+    """
+    use_cache=True，表示当多个依赖有一个共同的子依赖时，每次request请求只会调用子依赖一次，多次调用将从缓存中获取
+    """
+    return {"sub_dependency": final_query}
+
+
+"""Dependencies in path operation decorators 路径操作装饰器中的多依赖"""
+
+
+async def verify_token(x_token: str = Header(...)):
+    """没有返回值的依赖"""
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+    return x_token
