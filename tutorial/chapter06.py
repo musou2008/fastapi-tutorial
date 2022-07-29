2ce692c66ef47e01fec0be42237e37e9c1810c6c
musou2008 <deco.musou@gmail.com>
2022-07-29 16:13:35 +0800

    update

--- tutorial/chapter06.py
+++ tutorial/chapter06.py
@@ -1,7 +1,12 @@
+from collections import UserDict
+from datetime import datetime, timedelta
+from typing import Optional
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from pydantic import BaseModel
-from typing import Optional
 
 app06 = APIRouter()
 
@@ -12,6 +17,7 @@ OAuth2PasswordBearer是接收URL作为参数的一个类：客户端会向该URL
 OAuth2PasswordBearer并不会创建相应的URL路径操作，只是指明客户端用来请求Token的URL地址
 当请求到来的时候，FastAPI会检查请求的Authorization头信息，如果没有找到Authorization头信息，或者头信息的内容不是Bearer token，它会返回401状态码(UNAUTHORIZED)
 """
+
 oauth2_schema = OAuth2PasswordBearer(
     tokenUrl="/chapter06/token"
 )  # 请求Token的URL地址 http://127.0.0.1:8000/chapter06/token
@@ -23,6 +29,7 @@ async def oauth2_password_bearer(token: str = Depends(oauth2_schema)):
 
 
 """基于 Password 和 Bearer token 的 OAuth2 认证"""
+
 fake_users_db = {
     "john snow": {
         "username": "john snow",
@@ -54,6 +61,24 @@ class User(BaseModel):
 
 class UserInDB(User):
     hashed_password: str
+
+
+@app06.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+        )
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Incorrect username or password",
+        )
+    return {"access_token": user.username, "token_type": "bearer"}
 
 
 def get_user(db, username: str):
@@ -88,19 +113,66 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     return current_user
 
 
-@app06.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Incorrect username or password",
-        )
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Incorrect username or password",
-        )
-    return {"access_token": user.username, "token_type": "bearer"}
+@app06.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+"""OAuth2 with Password (and hashing), Bearer with JWT tokens 开发基于JSON Web Tokens的认证"""
+
+fake_users_db.update(
+    {
+        "john snow": {
+            "username": "john snow",
+            "full_name": "John Snow",
+            "email": "johnsnow@example.com",
+            "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+            "disabled": False,
+        }
+    }
+)
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"  # 生成密钥 openssl rand -hex 32
+ALGORITHM = "HS256"  # 算法
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 访问令牌过期分钟
+
+
+class Token(BaseModel):
+    """返合给用户的Token"""
+
+    access_token: str
+    token_type: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="/chapter06/jwt/token")
+
+
+def verity_password(plain_password: str, hashed_password: str):
+    """对密码进行校验"""
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def jwt_get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def jwt_authenticate_user(db, username: str, password: str):
+    user = jwt_get_user(db=db, username=username)
+    if not user:
+        return False
+    if not verity_password(
+        plain_password=password, hashed_password=user.hashed_password
+    ):
+        return False
+    return user
+
+
+def created_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
